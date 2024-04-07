a885672d1c8a2326e669ae5d5e730fca8e06f83a
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-04-08 02:48:39 +0630

    created: product apis && updated: category apis

--- src/app/api/v1/product-categories/[categoryId]/route.ts
+++ src/app/api/v1/product-categories/[categoryId]/route.ts
@@ -5,49 +5,64 @@ type paramsType = { params: { categoryId: string } };
 
 /* GET /api/v1/product-categories/:categoryId */
 export async function GET(req: NextRequest, { params }: paramsType) {
-    const productCategory = await prisma.productCategory.findUnique({
-        where: {
-            productCategoryId: params.categoryId,
-        },
-    });
+    try {
+        const productCategory = await prisma.productCategory.findUnique({
+            where: {
+                productCategoryId: params.categoryId,
+            },
+        });
 
-    if (!productCategory)
-        return NextResponse.json({ message: "Product category not found." }, { status: 404 });
+        if (!productCategory)
+            return NextResponse.json({ message: "Product category not found." }, { status: 404 });
 
-    return NextResponse.json(productCategory);
+        return NextResponse.json(productCategory, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Can't get the category." }, { status: 500 });
+    }
 }
 
 /* PUT /api/v1/product-categories/:categoryId */
 export async function PUT(req: NextRequest, { params }: paramsType) {
-    const body = await req.json();
+    try {
+        const body = await req.json();
 
-    const updatedCategory = await prisma.productCategory.update({
-        where: {
-            productCategoryId: params.categoryId,
-        },
-        data: {
-            productCategoryName: body.productCategoryName,
-            productCategoryCode: body.productCategoryCode,
-        },
-    });
+        const updatedCategory = await prisma.productCategory.update({
+            where: {
+                productCategoryId: params.categoryId,
+            },
+            data: {
+                productCategoryName: body.productCategoryName,
+                productCategoryCode: body.productCategoryCode,
+            },
+        });
 
-    if (!updatedCategory)
-        return NextResponse.json({ message: "Product category not found." }, { status: 404 });
+        if (!updatedCategory)
+            return NextResponse.json({ message: "Product category not found." }, { status: 404 });
 
-    return NextResponse.json(updatedCategory);
+        return NextResponse.json(updatedCategory, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Can't update the category." }, { status: 500 });
+    }
 }
 
 /* DELETE /api/v1/product-categories/:categoryId */
 export async function DELETE(req: NextRequest, { params }: paramsType) {
-    const deletedCategory = await prisma.productCategory.delete({
-        where: { productCategoryId: params.categoryId },
-    });
+    try {
+        const deletedCategory = await prisma.productCategory.delete({
+            where: { productCategoryId: params.categoryId },
+        });
 
-    if (!deletedCategory)
-        return NextResponse.json({ message: "Product category not found." }, { status: 404 });
+        if (!deletedCategory)
+            return NextResponse.json({ message: "Product category not found." }, { status: 404 });
 
-    return NextResponse.json({
-        message: "Product category deleted.",
-        productCategoryId: deletedCategory.productCategoryId,
-    });
+        return NextResponse.json(
+            {
+                message: "Product category deleted.",
+                productCategoryId: deletedCategory.productCategoryId,
+            },
+            { status: 200 },
+        );
+    } catch (error) {
+        return NextResponse.json({ message: "Can't delete the category." }, { status: 500 });
+    }
 }
---
+++ src/app/api/v1/products/route.ts
@@ -0,0 +1,34 @@
+import prisma from "@/db/prismaClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const products = await prisma.Product.findMany();
+        return NextResponse.json(products, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Something went wrong." }, { status: 500 });
+    }
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const newProduct = await prisma.Product.create({
+            data: {
+                productCode: body.productCode,
+                productName: body.productName,
+                price: body.price,
+                category: {
+                    connect: {
+                        productCategoryCode: body.categoryCode,
+                    },
+                },
+                saleInvoiceDetails: body?.saleInvoiceDetails,
+            },
+        });
+        return NextResponse.json(newProduct, { status: 201 });
+    } catch (error) {
+        console.error("Error creating product:", error);
+        return NextResponse.json({ message: "Can not create the product" }, { status: 500 });
+    }
+}
