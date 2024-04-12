8b7e95dd4e2b2b8b4a5372a861de049a12c8b0f1
mayzinei <mze1001web@gmail.com>
2024-04-12 20:06:38 +0630

    adding dashboard cards

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+"use client";
+import Card from "@/components/pages/dashboard/card/card";
+import { TrendingUp, LineChart, UsersRound, Boxes } from "lucide-react";
+const cardData = [
+    {
+        icon: <TrendingUp />,
+        title: "today sales",
+        amount: "4,955",
+    },
+    {
+        icon: <LineChart />,
+        title: "total sales",
+        amount: "896,500",
+    },
+    {
+        icon: <Boxes />,
+        title: "products",
+        amount: "6,225",
+    },
+    {
+        icon: <UsersRound />,
+        title: "total customers",
+        amount: "1,225",
+    },
+];
 
 export default function Dashboard() {
-    return <div className="h-screen w-full bg-green-200">Dashboard</div>;
+    return (
+        <div className="w-full">
+            <div className="grid grid-cols-5 gap-4">
+                {cardData.map((data, index) => (
+                    <Card key={index} icon={data.icon} title={data.title} amount={data.amount} />
+                ))}
+            </div>
+        </div>
+    );
 }
