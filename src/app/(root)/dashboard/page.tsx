521f34e72e12d3b51c2308b7e858f83c6e3bef14
mayzinei <mze1001web@gmail.com>
2024-04-12 20:59:57 +0630

    add graph and chart

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Card from "@/components/pages/dashboard/card/card";
-import { TrendingUp, LineChart, UsersRound, Boxes } from "lucide-react";
+import Chart from "@/components/pages/dashboard/chart/chart";
+import Graph from "@/components/pages/dashboard/graph/graph";
+import { TrendingUp, LineChart, UsersRound, Boxes, UserRound } from "lucide-react";
 const cardData = [
     {
         icon: <TrendingUp />,
@@ -18,6 +20,11 @@ const cardData = [
         amount: "6,225",
     },
     {
+        icon: <UserRound />,
+        title: "new customers",
+        amount: "225",
+    },
+    {
         icon: <UsersRound />,
         title: "total customers",
         amount: "1,225",
@@ -26,11 +33,19 @@ const cardData = [
 
 export default function Dashboard() {
     return (
-        <div className="w-full">
-            <div className="grid grid-cols-5 gap-4">
+        <div className="grid w-full grid-cols-2 gap-8">
+            <div className="col-span-2 grid grid-cols-5 gap-4">
                 {cardData.map((data, index) => (
                     <Card key={index} icon={data.icon} title={data.title} amount={data.amount} />
                 ))}
+            </div>
+            <div className="h-[300px] w-full space-y-4">
+                <h3 className="text-2xl font-bold">Daily Sale Report</h3>
+                <Graph />
+            </div>
+            <div className="h-[300px] w-full space-y-4">
+                <h3 className="text-2xl font-bold">Monthly Overview</h3>
+                <Chart />
             </div>
         </div>
     );
