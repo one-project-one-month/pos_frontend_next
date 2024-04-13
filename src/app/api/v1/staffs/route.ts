063bc64aea571616fe60c11e95ac80ee6fa67ff4
nainghtetlinn <naing.work.it@gmail.com>
2024-04-13 17:51:05 +0630

    update: staff create

--- src/app/api/v1/staffs/route.ts
+++ src/app/api/v1/staffs/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/db/prismaClient";
+
+import prisma, { exclude } from "@/db/prismaClient";
 import { catchAsyncError } from "@/lib/errorhandler";
+import { createStaffSchema } from "@/validations/staff";
+import { hashPassword } from "./utils";
 
 /* GET /api/v1/staffs */
 export async function GET(req: NextRequest) {
@@ -32,28 +35,23 @@ export async function POST(req: NextRequest) {
     const response = await catchAsyncError("[STAFF_POST]", async () => {
         const body = await req.json();
 
-        // TODO: password hashing logic here
+        const validation = createStaffSchema.safeParse(body);
 
+        if (!validation.success)
+            return NextResponse.json(
+                { message: "Invalid staff inputs.", error: validation.error.format() },
+                { status: 400 },
+            );
+
+        const hashed_password = await hashPassword(validation.data.password);
         const newStaff = await prisma.staff.create({
-            data: {
-                staffCode: body.staffCode,
-                staffName: body.staffName,
-                dateOfBirth: new Date(body.dateOfBirth),
-                mobileNo: body.mobileNo,
-                gender: body.gender,
-                address: body.address,
-                position: body.position,
-                // password: hashedPassword,
-                password: body.password,
-            },
+            data: { ...validation.data, password: hashed_password },
         });
 
         return NextResponse.json(
             {
                 message: "success",
-                data: {
-                    staff: newStaff,
-                },
+                data: { staff: exclude(newStaff, "password") },
             },
             { status: 201 },
         );
